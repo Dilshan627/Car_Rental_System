a491a53ffbdcd3d19ef60ecdc447d63fdbba1ab3
dilshan27 <damithaadilshan@gmail.com>
2023-02-13 00:29:08 +0530

    update

--- Back-End/src/main/java/lk/CarRent/Main.java
+++
@@ -1,7 +0,0 @@
-package lk.CarRent;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ Back-End/src/main/java/lk/CarRent/WebAppInitializer.java
@@ -0,0 +1,4 @@
+package lk.CarRent;
+
+public class WebAppInitializer {
+}
