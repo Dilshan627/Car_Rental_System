58693a5bff188553702f496ace88c3f6c994588b
dilshan27 <damithaadilshan@gmail.com>
2023-02-14 12:30:16 +0530

    frontend update

---
+++ Front-End/controller/DashBoardController.js
@@ -0,0 +1,14 @@
+$("#login_screen").css('display','none');
+
+$("#btn-login").click(function () {
+    $('#login_page_container').css('display','none');
+
+    $("#login_screen").css('display','block');
+});
+
+
+$("#back-dash-board").click(function () {
+    $('#login_page_container').css('display','block');
+
+    $("#login_screen").css('display','none');
+});
