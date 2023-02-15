cfd7a1b987646829a6794b39f3c1a497363a3ada
dilshan27 <damithaadilshan@gmail.com>
2023-02-15 19:12:08 +0530

    backend update

--- Back-End/src/main/java/lk/CarRent/WebAppInitializer.java
+++ Back-End/src/main/java/lk/CarRent/WebAppInitializer.java
@@ -1,20 +1,22 @@
 package lk.CarRent;
 
+import lk.CarRent.config.WebAppConfig;
+import lk.CarRent.config.WebRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
---
+++ Back-End/src/main/java/lk/CarRent/config/WebAppConfig.java
@@ -0,0 +1,7 @@
+package lk.CarRent.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebAppConfig {
+}
