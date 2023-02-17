ab2a7c88b369f25340bdb11fc4a8e8228903c34e
dilshan27 <damithaadilshan@gmail.com>
2023-02-17 07:59:39 +0530

    frontend update

--- Front-End/controller/NavigationController.js
+++ Front-End/controller/NavigationController.js
@@ -156,13 +156,3 @@ $("#log-dash-board-customer").click(function () {
     $("#driver_screen").css('display', 'none');
 });
 
-$("#btn_customer_register").click(function () {
-    $('#customer_register_screen').css('display', 'block');
-
-    $("#driver_register_screen").css('display', 'none');
-});
-$("#btn_driver_register").click(function () {
-    $('#customer_register_screen').css('display', 'none');
-
-    $("#driver_register_screen").css('display', 'block');
-});
