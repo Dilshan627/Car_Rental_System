4dafe48861b33a465df2133617a3c96532850b66
dilshan27 <damithaadilshan@gmail.com>
2023-02-17 20:35:43 +0530

    frontend update

--- Front-End/controller/NavigationController.js
+++ Front-End/controller/NavigationController.js
@@ -110,6 +110,9 @@ $("#btn-login").click(function () {
     $("#login_screen").css('display', 'block');
 
     $('#loading_page').css('display', 'none');
+
+    $('#userName').val("");
+    $('#password').val("");
 });
 
 $("#btn-signUp").click(function () {
@@ -138,33 +141,7 @@ $("#back-dash-board").click(function () {
     $("#driver_screen").css('display', 'none');
 });
 
-$("#log-dash-board-admin").click(function () {
-    $("#admin_screen").css('display', 'block');
 
-    $('#loading_page').css('display', 'none');
-    $('#nav-bar').css('display', 'none');
-    $("#login_screen").css('display', 'none');
-    $("#driver_screen").css('display', 'none');
-    $("#customer_screen").css('display', 'none');
 
-});
 
-$("#log-dash-board-driver").click(function () {
-    $("#driver_screen").css('display', 'block');
-
-    $('#loading_page').css('display', 'none');
-    $("#login_screen").css('display', 'none');
-    $("#admin_screen").css('display', 'none');
-    $("#customer_screen").css('display', 'none');
-
-});
-
-$("#log-dash-board-customer").click(function () {
-    $("#customer_screen").css('display', 'block');
-
-    $('#loading_page').css('display', 'none');
-    $("#login_screen").css('display', 'none');
-    $("#admin_screen").css('display', 'none');
-    $("#driver_screen").css('display', 'none');
-});
 
---
+++ Front-End/controller/loginController.js
@@ -0,0 +1,37 @@
+let baseURL = "http://localhost:8080/Back_End_war/";
+$("#log-dash-board-customer").click(function () {
+
+    /* $("#customer_screen").css('display', 'block');
+
+     $('#loading_page').css('display', 'none');
+     $("#login_screen").css('display', 'none');
+     $("#admin_screen").css('display', 'none');
+     $("#driver_screen").css('display', 'none');*/
+
+    let userName = $("#userName").val();
+    let password = $("#password").val();
+
+
+
+});
+
+$("#log-dash-board-admin").click(function () {
+    $("#admin_screen").css('display', 'block');
+
+    $('#loading_page').css('display', 'none');
+    $('#nav-bar').css('display', 'none');
+    $("#login_screen").css('display', 'none');
+    $("#driver_screen").css('display', 'none');
+    $("#customer_screen").css('display', 'none');
+
+});
+
+$("#log-dash-board-driver").click(function () {
+    $("#driver_screen").css('display', 'block');
+
+    $('#loading_page').css('display', 'none');
+    $("#login_screen").css('display', 'none');
+    $("#admin_screen").css('display', 'none');
+    $("#customer_screen").css('display', 'none');
+
+});
