f9d26bf47b7b557ef5151bc3d8e726e4711fcfde
dilshan27 <damithaadilshan@gmail.com>
2023-02-21 08:13:14 +0530

    backend update

--- Back-End/src/main/java/lk/CarRent/service/impl/CarServiceImpl.java
+++ Back-End/src/main/java/lk/CarRent/service/impl/CarServiceImpl.java
@@ -1,9 +1,11 @@
 package lk.CarRent.service.impl;
 
 import lk.CarRent.dto.CarDTO;
+import lk.CarRent.entity.Car;
 import lk.CarRent.repo.CarRepo;
 import lk.CarRent.service.CarService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,26 +26,45 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public void saveCar(CarDTO car) {
-
+        if (!carRepo.existsById(car.getRegistrationNO())) {
+            carRepo.save(mapper.map(car, Car.class));
+        } else {
+            throw new RuntimeException(car.getRegistrationNO() + " " + "Car Already Exists..!");
+        }
     }
 
     @Override
     public void updateCar(CarDTO car) {
-
+        if (carRepo.existsById(car.getRegistrationNO())) {
+            carRepo.save(mapper.map(car, Car.class));
+        } else {
+            throw new RuntimeException(car.getRegistrationNO() + " " + "No Such Car..! Please Check The Correct Id..!");
+        }
     }
 
     @Override
     public void deleteCar(String id) {
-
+        if (carRepo.existsById(id)) {
+            carRepo.deleteById(id);
+        } else {
+            throw new RuntimeException(id + " " + "No Such Car..! Please Check The Correct Id..!");
+        }
     }
 
     @Override
     public CarDTO searchCar(String id) {
-        return null;
+        if (carRepo.existsById(id)) {
+            Car car = carRepo.findById(id).get();
+            return mapper.map(car, CarDTO.class);
+        } else {
+            throw new RuntimeException(id + " " + "No Such Car..! Please Check The Id..!");
+        }
     }
 
     @Override
     public List<CarDTO> getAllCar() {
-        return null;
+        List<Car> all = carRepo.findAll();
+        return mapper.map(all, new TypeToken<List<CarDTO>>() {
+        }.getType());
     }
 }
