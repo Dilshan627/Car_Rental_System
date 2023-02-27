5e69daafeb24af8899843501f418e28a5bf6118d
dilshan27 <damithaadilshan@gmail.com>
2023-02-27 16:16:33 +0530

    frontend update

---
+++ Front-End/controller/IncomeController.js
@@ -0,0 +1,20 @@
+incomeLoad();
+
+function incomeLoad() {
+    $("#incomeTable").empty();
+
+    $.ajax({
+        url: baseURL + "payment",
+        dataType: "json",
+        success: function (resp) {
+            for (let pay of resp.data) {
+                var row = '<tr><td>' + pay.paymentId + '</td><td>' + pay.bookingId + '</td><td>' + pay.paymentDate + '</td><td>' + pay.rentFee
+                    + '</td><td>' + pay.driverFee + '</td><td>' + pay.lossPayment + '</td><td>' + pay.fullPayment + '</td></tr>';
+                $("#incomeTable").append(row);
+
+            }
+            paymentID();
+
+        }
+    });
+}
